1b701bf92116dad4c1a1499c267786319c322cc6
Alex Moraes de Oliveira <alxmoliveira@gmail.com>
2022-05-17 16:42:00 -0300

    First commit

---
+++ scripts/die.js
@@ -0,0 +1,27 @@
+let images = ["assets/svg/dice-01.svg",
+"assets/svg/dice-02.svg",
+"assets/svg/dice-03.svg",
+"assets/svg/dice-04.svg",
+"assets/svg/dice-05.svg",
+"assets/svg/dice-06.svg"];
+let dice = document.querySelectorAll(".die img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+        console.log(dieOneValue,dieTwoValue);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+        document.querySelector("#total").innerHTML = "Sua rolagem deu " + ( (dieOneValue +1) + (dieTwoValue + 1) );
+    },
+    1000
+    );
+}
+roll();
